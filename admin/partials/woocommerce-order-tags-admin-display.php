9ff7a591f1a890991447a8aaa252c9c375672310
Spencer Walden <spencer@chocolab.com.au>
2018-01-22 15:35:44 +1100

    Added TODO item to readme and removed slugs

--- admin/partials/woocommerce-order-tags-admin-display.php
+++ admin/partials/woocommerce-order-tags-admin-display.php
@@ -12,8 +12,6 @@
 					var newField = "<div id = 'tagGroup" + count + "'><span class = 'woocommerce_tags_section'>" +
 					"<span>Tag Title</span>" +
 					"<input type='text' name='woocommerce_tags_settings[woocommerce_tags_field][" + count + "][name]' value = ''>" +
-					"<span>Tag Slug</span>" +
-					"<input type='text' name='woocommerce_tags_settings[woocommerce_tags_field][" + count + "][slug]' value = ''>" +
 				"</span><button type = 'button' onclick = 'removeSection('#tagGroup'" + count + ");'>Remove Tag</button></div>"
 					parent.append(newField);
 					jQuery("#submit").click();
@@ -29,8 +27,8 @@
 						<span class = "woocommerce_tags_section">
 						<span>Tag Title</span>
 							<input type='text' name='woocommerce_tags_settings[woocommerce_tags_field][<?php echo $i; ?>][name]' value='<?php echo $options['woocommerce_tags_field'][$i]['name']; ?>'>
-							<span>Tag Slug</span>
-							<input type='text' name='woocommerce_tags_settings[woocommerce_tags_field][<?php echo $i; ?>][slug]' value='<?php echo $options['woocommerce_tags_field'][$i]['slug']; ?>'>
+
+					
 					</span>
 				<button type = "button" onclick = 'removeSection("#tagGroup<?php echo $i; ?>")'>Remove Tag</button>
 			</div>
@@ -41,8 +39,7 @@
 						<span id = "fields" class = "woocommerce_tags_section">
 							<span>Tag Title</span>
 							<input type='text' name='woocommerce_tags_settings[woocommerce_tags_field][0][name]'>
-							<span>Tag slug</span>
-							<input type='text' name='woocommerce_tags_settings[woocommerce_tags_field][0][slug]'>
+							
 						</span>
 						<button type = "button" onclick = 'removeSection("#tagGroup<?php echo $i; ?>")'>Remove Tag</button>
 					</div>
